8c062c521f3a06d23377d4efde68261abc50f006
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2018-07-31 21:46:09 +0700

    Add some test in dummy data

--- src/test/Dummy.spec.ts
+++ src/test/Dummy.spec.ts
@@ -4,12 +4,21 @@ import Flattenr from "../Flattenr";
 import dummy = require("./data/dummy.json");
 
 describe("Dummy data", () => {
-  const flatData = new Flattenr(dummy, "/");
+  const separator = "/";
+  const flatData = new Flattenr(dummy, separator);
 
-  it("should instance of Flattenr", () =>
-    expect(flatData).instanceof(Flattenr));
+  it("should instance of Flattenr and has same separator", () => {
+    expect(flatData).instanceof(Flattenr);
+    expect(flatData.separator).to.be.equal(separator);
+  });
 
   it("should contain 6/age with value 29", () => {
     expect(flatData.get("6/age")).to.be.equal(29);
+  });
+
+  it("should capable to find with string, regexp and object", () => {
+    const twoThousandsBalance = flatData.find({ balance: "$2," });
+
+    expect(Object.keys(twoThousandsBalance).length).to.be.equal(4);
   });
 });
